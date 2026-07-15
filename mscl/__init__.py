380934a9c9fc9a0fcb60167a37d9e699e6f231c6
samcool1224 <samaksh2405@gmail.com>
2026-07-15 13:10:53 -0400

    Add new imports for Z3 backend and sample search

--- mscl/__init__.py
+++ mscl/__init__.py
@@ -5,6 +5,11 @@ from .relations import desugar, ALL_RELATIONS, arity
 from .json_io import spec_from_json, spec_to_json, node_to_json, json_schema
 from .render import to_spring, from_spring
 from .feasibility import model_check, feasible, collect_atoms, init_domains
+from .z3_backend import (Z3Backend, UnsatExplanation, SolverUnavailableError,
+                         SolverUnknownError, z3_available)
+from .samplesearch import (SampleSearch, SampleResult, SearchStats, SearchEvent,
+                           PreferenceModel, UniformPreference, GeometricPreference,
+                           UnsatError, generate_layout)
 from .validate import validate, assert_resolved, ValidationError
 from .dialogue import resolve, resolve_ask_none, resolve_ask_all, Question, ResolutionLog
 from . import profile
@@ -19,6 +24,10 @@ __all__ = [
     "spec_from_json", "spec_to_json", "node_to_json", "json_schema",
     "to_spring", "from_spring",
     "model_check", "feasible", "collect_atoms", "init_domains",
+    "Z3Backend", "UnsatExplanation", "SolverUnavailableError", "SolverUnknownError",
+    "z3_available", "SampleSearch", "SampleResult", "SearchStats", "SearchEvent",
+    "PreferenceModel", "UniformPreference", "GeometricPreference", "UnsatError",
+    "generate_layout",
     "validate", "assert_resolved", "ValidationError",
     "resolve", "resolve_ask_none", "resolve_ask_all", "Question", "ResolutionLog",
     "profile",
